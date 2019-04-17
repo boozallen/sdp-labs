247c14105181738a5fd80f42e177e7564dbd771f
Carlos OKieffe <okieffe_carlos@bah.com>
2019-04-17 17:53:49 -0400

    changed sonarqube build step behavior; build_step and require_build_step

--- tiers/sonarqube-build-before/pipeline_config.groovy
+++ tiers/sonarqube-build-before/pipeline_config.groovy
@@ -11,7 +11,8 @@ libraries{
   }
   github_enterprise
   sonarqube{
-    require_build_source = true
+    require_build_step = true
+    build_step = "build_source"
     enforce_quality_gate = true
   }
 
