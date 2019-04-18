ea9cbc12188c749de7881a1ba8f0b2076e11f98f
Carlos OKieffe <okieffe_carlos@bah.com>
2019-04-18 12:01:03 -0400

    using not build_step but require_build_step; retesting with JTE plugin update

--- tiers/sonarqube-build-before/pipeline_config.groovy
+++ tiers/sonarqube-build-before/pipeline_config.groovy
@@ -11,8 +11,8 @@ libraries{
   }
   github_enterprise
   sonarqube{
-//    require_build_step = true
-    build_step = "build_source"
+    require_build_step = true
+//    build_step = "build_source"
     enforce_quality_gate = true
   }
 
